12002fb5e5a439a78e7ba358d51c8842319f778a
unknown <gal@GAL-T440S.searsil.loc>
2015-08-24 17:36:48 +0300

    adding JS that changes the grants diaplay

--- calculatingStuff.js
+++ calculatingStuff.js
@@ -18,14 +18,28 @@ var projects;
 
 // Attach an asynchronous callback to read the data at our posts reference
 ref.on("value", function(snapshot) {
-                  console.log(snapshot.val());
-                  angels = snapshot.val().angels;
-                  projects = snapshot.val().projects;
+               console.log(snapshot.val());
+               angels = snapshot.val().angels;
+               projects = snapshot.val().projects;
+               showProjects();      
    },
    function (errorObject) {
         console.log("The read failed: " + errorObject.code);
    }
 );
+
+function showProjects(){
+    for (var project=0; project<projects.length; project++){  
+      $('body').append('<div class="projectDisp" id="project'+ project + '"/>');
+      $('#project' + project).append('<div class="projectNames" id="projectName' + project + '"/>');
+      $('#projectName' + project).append('<div class="projectTytle" id="projectTytle' + project + '">' + projects[project].name + '</div>');
+      $('#projectName' + project).append('<div class="projectDesc" id="projectDesc' + project + '">'+ projects[project].desc +'</div></div>');                    
+      $('#project' + project).append('<div class="grantsBar" id="grantsBar' + project + '"/></div>');
+      for (var angel=0; angel < angels.length; angel++){
+        $('#grantsBar' + project).append('<div class="grantMark" id="grantMarkProject' + project + 'Angel' + angel + '"/>');
+      }
+    }
+}
 
 function calculateAngelsBalance(){
   angelsBalance = [limit,limit,limit,limit,limit,limit];
@@ -39,23 +53,13 @@ function calculateAngelsBalance(){
       }
     }  
   }
-  
-//  for (var angelId=0; angelId<angels.length; angelId++){
-  //  document.getElementById("angelsBalance").innerHTML += angels[angelId].name + "'s balance is " + angelsBalance[angelId] + '</br>';
-//  }
+
 }
   
 function calculateProjectsBalance(){
- // document.getElementById("projectsBalance").innerHTML += '</br>';
   for (var projectId=0; projectId<projects.length; projectId++){
     balance[projectId].balance = 0;
-  /*  if (typeof projects[projectId].grantsDist !== "undefined"){
-      for (var grantId=0; grantId<projects[projectId].grantsDist.length; grantId++){   
-        balance[projectId].balance += projects[projectId].grantsDist[grantId].sum;
-      }
-    }
-    document.getElementById("projectsBalance").innerHTML += projects[projectId].name + "'s balance is " +balance[projectId].balance + '</br>';
- */ }
+ }
 }
 
 function rankProjects(){
@@ -81,19 +85,17 @@ function isTheSecondvalueBigger(firstValue,secondValue) {
   }
 }
 
-function showRankProjects(){
-  rankProjects();
-  console.log("starting showRankProjects")
- //for (var project=projects.length -1 ; project > -1 ; project--){
-    //console.log("project = " + project + ", in balance it's the " + balance[project].projectId)
-   // var Id = balance[project].projectId;
-    //document.getElementById("rankedProjects").innerHTML += projects[Id].name + "'s balance is " + balance[project].balance + '</br>';
-  for (var project=0; project<projects.length; project++){  
-    $('body').append('<div class="projectDisp" id="project'+ project + '"/>');
-    $('#project' + project).append('<div class="projectNames" id="projectName' + project + '"/>');
-    $('#projectName' + project).append('<div class="projectTytle" id="projectTytle' + project + '">Project ' + project + "'s name</div>");
-    $('#projectName' + project).append('<div class="projectDesc" id="projectDesc' + project + '">This is project number' + project + '. Look at it shine!</div></div>');
-    $('#project' + project).append('<div class="grantsBar" id="grantsBar' + project + '"/></div>');
-  }
+function showNextAngelGrant(projectId,angel){
+    for (var grantId=0; grantId<projects[projectId].grantsDist.length; grantId++){
+      if (projects[projectId].grantsDist[grantId].angelId == angel){
+        var grant = projects[projectId].grantsDist[grantId];
+        var percentage = grant.sum*100/limit;
+        $('#grantMarkProject' + projectId + 'Angel' + angel).css({
+            "width" : percentage + "%",
+            "background-color" : angels[angel].color,
+            "border": "1px solid black",
+        });
+      }
+    }  
 }
 
