2c6688136d0a2cc83b07a8db28225f0226aeec0f
unknown <gal@GAL-T440S.searsil.loc>
2015-08-25 12:43:32 +0300

    Showing the grants one by one

--- calculatingStuff.js
+++ calculatingStuff.js
@@ -10,22 +10,28 @@ var balance = [
   {"projectId":5,"balance":0}
   ];
 
+var angelProject = {"lasnAngel": 0, "lastProject":0};
+
 var ref = new Firebase("https://searsangels.firebaseio.com/");
 var angels;
 var projects;
-
 
 // Attach an asynchronous callback to read the data at our posts reference
 ref.on("value", function(snapshot) {
                console.log(snapshot.val());
                angels = snapshot.val().angels;
                projects = snapshot.val().projects;
-               showProjects();      
+               showProjects();     
+               calculateMaxGrant(); 
    },
    function (errorObject) {
         console.log("The read failed: " + errorObject.code);
    }
 );
+
+function click(event) {
+    alert('Clicked. The event is' + event);
+}
 
 function showProjects(){
     for (var project=0; project<projects.length; project++){  
@@ -89,11 +95,23 @@ function isTheSecondvalueBigger(firstValue,secondValue) {
   }
 }
 
+function showNextGrant(){
+  if (angelProject.lasnAngel < angels.length){
+    showNextAngelGrant(angelProject.lastProject, angelProject.lasnAngel);
+    angelProject.lasnAngel +=1;
+  }
+  else {
+    angelProject.lastProject +=1
+    angelProject.lasnAngel = 0;
+    showNextAngelGrant(angelProject.lastProject, angelProject.lasnAngel);
+    angelProject.lasnAngel +=1;
+  }
+}
+
 function showNextAngelGrant(projectId,angel){
     for (var grantId=0; grantId<projects[projectId].grantsDist.length; grantId++){
       if (projects[projectId].grantsDist[grantId].angelId == angel){
         var grant = projects[projectId].grantsDist[grantId];
-        calculateMaxGrant();
         var percentage = grant.sum*100/totalMax;
         $('#grantMarkProject' + projectId + 'Angel' + angel).css({
             "width" : percentage + "%",
@@ -106,6 +124,5 @@ function showNextAngelGrant(projectId,angel){
 
 function calculateMaxGrant(){
   rankProjects();
-  totalMax = balance[5].balance;
+  totalMax = balance[5].balance + 1000;
 }
-
